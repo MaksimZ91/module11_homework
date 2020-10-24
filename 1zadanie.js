4885087bfb2e77e30738b45669ea6f0317330d51
Iuliia Tokarevskaia <iuliia.tokarevskaia@socialbakers.com>
2020-10-24 12:23:07 +0200

    Проверено

--- 1zadanie.js
+++ 1zadanie.js
@@ -4,9 +4,11 @@ function numberOfEvenInArray(arr3) {
     let nol = 0
     for (let i = 0; i < arr3.length; i++) {
         if (typeof arr3[i] === "number" && !isNaN(arr3[i])) {
-            arr3[i] % 2 === 0 ? chet++ : nechet++
-            if (arr3[i] === 0)
+            if (arr3[i] === 0) {
                 nol++
+            } else {
+                arr3[i] % 2 === 0 ? chet++ : nechet++
+            }
         }
     }
     console.log(`Количество  нулевых ${nol} элементов.`)
@@ -16,10 +18,4 @@ function numberOfEvenInArray(arr3) {
 
 numberOfEvenInArray([2,0,4,6,3,NaN,null, "дом",3,4,undefined])
 
-
-
-
-
-
-
-
+// Почти правильно, но проверка на ноль должна стоять раньше, чем проверка на чет\нечет, т.к. остаток от деления 0 на 2 также равен нулю, поэтому нули сейчас учитываются дважды - и как четное число, и как ноль. Выше исправила на верный вариант
